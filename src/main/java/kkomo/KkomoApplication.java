e2b44d907461b0bedc8e28d61fddc625ae1d84b8
bokyeong <wntjdals0412@gmail.com>
2024-12-10 21:27:56 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/kkomo/KkomoApplication.java
@@ -0,0 +1,13 @@
+package kkomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkomoApplication.class, args);
+	}
+
+}
