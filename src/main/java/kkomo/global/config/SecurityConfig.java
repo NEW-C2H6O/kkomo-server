18bbc8a9c268b85de68c523859840afc74343757
bokyeong <wntjdals0412@gmail.com>
2024-12-29 17:48:00 +0900

    KM-7 feat : 카카오 로그인 기능 구현

---
+++ src/main/java/kkomo/global/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package kkomo.global.config;
+
+import kkomo.security.service.Oauth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final Oauth2UserService oauth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-resources",
+                                "/actuator/**",
+                                "/test/signup",
+                                "/",
+                                "/ott",
+                                "/ws/**",
+                                "/auth/login/**",
+                                "/oauth2/**"
+
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .failureUrl("/login?error")
+                        .redirectionEndpoint(redirection -> redirection
+                                .baseUri("/login/oauth2/code/kakao"))
+                        .userInfoEndpoint(userInfoEndpoint ->
+                                userInfoEndpoint.userService(oauth2UserService)
+                        )
+                )
+                .sessionManagement(Customizer.withDefaults());
+        return http.build();
+    }
+}
