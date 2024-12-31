1002e80bc3906fbb5e490085a8d29d41b6b41e12
scv1702 <scv1702@naver.com>
2024-12-31 16:33:54 +0900

    KM-15 feat: 내 OTT 예약 내역 조회에 커서 기반 페이지네이션 추가

--- src/main/java/kkomo/reservation/controller/OTTReservationController.java
+++ src/main/java/kkomo/reservation/controller/OTTReservationController.java
@@ -1,18 +1,21 @@
 package kkomo.reservation.controller;
 
 import kkomo.global.ApiResponse;
+import kkomo.global.support.Cursor;
+import kkomo.global.support.CursorDefault;
+import kkomo.global.support.CursorPageable;
+import kkomo.global.support.SliceResponse;
 import kkomo.reservation.controller.dto.request.ReserveOTTRequest;
 import kkomo.reservation.controller.dto.response.GetOTTReservationResponse;
 import kkomo.reservation.controller.dto.response.ReserveOTTResponse;
-import kkomo.reservation.domain.OTTReservation;
+import kkomo.reservation.service.OTTReservationQueryService;
 import kkomo.reservation.service.OTTReservationService;
 import kkomo.reservation.service.command.ReserveOTTCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import static kkomo.global.ApiResponse.ApiSuccessResult;
 
@@ -23,6 +26,7 @@ public class OTTReservationController {
 
     private final OTTReservationCommandMapper mapper;
     private final OTTReservationService ottReservationService;
+    private final OTTReservationQueryService ottReservationQueryService;
 
     @PostMapping
     public ResponseEntity<ApiSuccessResult<ReserveOTTResponse>> reserveOTT(
@@ -37,13 +41,12 @@ public class OTTReservationController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<ApiSuccessResult<List<GetOTTReservationResponse>>> getMyOTTReservation() {
+    public ResponseEntity<ApiSuccessResult<SliceResponse<GetOTTReservationResponse>>> getMyOTTReservation(
+        @CursorDefault @PageableDefault final CursorPageable<Cursor> pageable
+    ) {
         // TODO: 세션을 통해 memberId를 가져오는 로직 추가 구현 필요
         final Long memberId = 1L;
-        final List<OTTReservation> result = ottReservationService.readMyBy(memberId);
-        final List<GetOTTReservationResponse> response = result.stream()
-            .map(GetOTTReservationResponse::from)
-            .toList();
+        final SliceResponse<GetOTTReservationResponse> response = ottReservationQueryService.readMyBy(memberId, pageable);
         return ApiResponse.success(HttpStatus.OK, response);
     }
 }
--- src/main/java/kkomo/reservation/repository/OTTReservationRepository.java
+++ src/main/java/kkomo/reservation/repository/OTTReservationRepository.java
@@ -1,7 +1,6 @@
 package kkomo.reservation.repository;
 
 import kkomo.reservation.domain.OTTReservation;
-import kkomo.reservation.domain.OTTReservationTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,16 +8,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OTTReservationRepository extends JpaRepository<OTTReservation, Long> {
-
-    @Query("select r.time from OTTReservation r " +
-            "where r.ott.id = :ottId and r.profile.id = :profileId and " +
-            "r.time.start >= :start and r.time.end <= :end")
-    List<OTTReservationTime> findByOttIdAndProfileIdAndTimeBetween(
-        Long ottId,
-        Long profileId,
-        LocalDateTime start,
-        LocalDateTime end
-    );
 
     @Query(value = "SELECT r.ott_id " +
         "FROM ott_reservation r " +
@@ -33,6 +22,4 @@ public interface OTTReservationRepository extends JpaRepository<OTTReservation, 
         LocalDateTime start,
         LocalDateTime end
     );
-
-    List<OTTReservation> findByMember_Id(Long memberId);
 }
