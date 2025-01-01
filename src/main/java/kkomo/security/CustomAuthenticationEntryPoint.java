a12eb67c9b6b4f7bf899b89163521d31de385b51
bokyeong <wntjdals0412@gmail.com>
2025-01-01 20:33:03 +0900

    KM-13 feat : 인증 안된 요청 처리

---
+++ src/main/java/kkomo/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package kkomo.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);  // 401 상태 코드
+        response.setContentType("application/json;charset=UTF-8");
+
+        String errorMessage = "로그인이 필요합니다.";
+        String jsonResponse = "{\"status\": \"401\", \"errors\": \"" + errorMessage + "\"}";
+
+        response.getWriter().write(jsonResponse);
+    }
+}
+
