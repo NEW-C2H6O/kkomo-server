876e98935377f29b07a87f9f8b9e958b3462a5d9
bokyeong <wntjdals0412@gmail.com>
2025-01-01 20:37:30 +0900

    KM-13 feat : 로그인 요청 api에만 로그인 허용

---
+++ src/main/java/kkomo/auth/controller/AuthController.java
@@ -0,0 +1,24 @@
+package kkomo.auth.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kkomo.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@Slf4j
+public class AuthController {
+    private final AuthService authService;
+    private final HttpServletRequest request;
+
+    @PostMapping("/login")
+    public RedirectView login() {
+        return new RedirectView("/v1/oauth2/authorization/kakao");
+    }
+}
