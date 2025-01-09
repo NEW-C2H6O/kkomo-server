039098a3d37b237dd3bc803ce966a09fa66a5344
scv1702 <scv1702@naver.com>
2025-01-10 00:08:56 +0900

    KM-26 refactor: 회원 정보 조회를 쿼리 서비스로 분리

--- src/main/java/kkomo/member/controller/MemberController.java
+++ src/main/java/kkomo/member/controller/MemberController.java
@@ -3,9 +3,9 @@ package kkomo.member.controller;
 import kkomo.auth.UserPrincipal;
 import kkomo.global.ApiResponse;
 import kkomo.member.controller.response.MemberResponse;
+import kkomo.member.service.MemberQueryService;
 import kkomo.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,24 +14,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static kkomo.global.ApiResponse.ApiSuccessResult;
+
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberQueryService memberQueryService;
 
     @GetMapping("/me")
-    public ResponseEntity<ApiResponse.ApiSuccessResult<MemberResponse>> loadInfo(
-            @AuthenticationPrincipal UserPrincipal principal) {
-        log.info("loadInfo {}", principal.getName());
-        return ApiResponse.success(HttpStatus.OK, memberService.getMemberInfo(principal.getId()));
+    public ResponseEntity<ApiSuccessResult<MemberResponse>> loadInfo(
+        @AuthenticationPrincipal final UserPrincipal principal
+    ) {
+        final MemberResponse response = memberQueryService.getMemberInfo(principal.getId());
+        return ApiResponse.success(HttpStatus.OK, response);
     }
 
     @DeleteMapping("/me")
-    public ResponseEntity<ApiResponse.ApiSuccessResult<String>> deleteInfo(@AuthenticationPrincipal UserPrincipal principal) {
+    public ResponseEntity<ApiSuccessResult<?>> deleteInfo(
+        @AuthenticationPrincipal final UserPrincipal principal
+    ) {
         memberService.deleteMember(principal.getId());
-        return ApiResponse.success(HttpStatus.NO_CONTENT, "성공적으로 탈퇴하였습니다.");
+        return ApiResponse.success(HttpStatus.NO_CONTENT);
     }
 }
