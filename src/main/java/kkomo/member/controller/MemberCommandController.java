7de52bbd68f18d7f117ffbb610296704a4e85437
scv1702 <scv1702@naver.com>
2025-01-10 22:59:09 +0900

    KM-28 refactor: 회원 탈퇴를 MemberCommandController로 분리

--- src/main/java/kkomo/member/controller/MemberCommandController.java
+++ src/main/java/kkomo/member/controller/MemberCommandController.java
@@ -2,15 +2,12 @@ package kkomo.member.controller;
 
 import kkomo.auth.UserPrincipal;
 import kkomo.global.ApiResponse;
-import kkomo.member.controller.response.MemberResponse;
-import kkomo.member.service.MemberQueryService;
-import kkomo.member.service.MemberService;
+import kkomo.member.service.MemberCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,24 +16,16 @@ import static kkomo.global.ApiResponse.ApiSuccessResult;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
-public class MemberController {
+public class MemberCommandController {
 
-    private final MemberService memberService;
-    private final MemberQueryService memberQueryService;
-
-    @GetMapping("/me")
-    public ResponseEntity<ApiSuccessResult<MemberResponse>> loadInfo(
-        @AuthenticationPrincipal final UserPrincipal principal
-    ) {
-        final MemberResponse response = memberQueryService.getMemberInfo(principal.getId());
-        return ApiResponse.success(HttpStatus.OK, response);
-    }
+    private final MemberCommandService memberCommandService;
 
     @DeleteMapping("/me")
-    public ResponseEntity<ApiSuccessResult<?>> deleteInfo(
+    public ResponseEntity<ApiSuccessResult<?>> withdrawMember(
         @AuthenticationPrincipal final UserPrincipal principal
     ) {
-        memberService.deleteMember(principal.getId());
+        final Long memberId = principal.getId();
+        memberCommandService.withdrawMember(memberId);
         return ApiResponse.success(HttpStatus.NO_CONTENT);
     }
 }
