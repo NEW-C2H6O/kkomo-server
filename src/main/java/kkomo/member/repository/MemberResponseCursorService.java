156b110d19e7f284180a15e940411468b93527d0
scv1702 <scv1702@naver.com>
2025-01-17 03:48:12 +0900

    KM-27 refactor: CursorServiceSupport 삭제

--- src/main/java/kkomo/member/repository/MemberResponseCursorService.java
+++ src/main/java/kkomo/member/repository/MemberResponseCursorService.java
@@ -3,31 +3,27 @@ package kkomo.member.repository;
 import kkomo.admin.controller.dto.response.MemberResponse;
 import kkomo.global.support.Base64Encoder;
 import kkomo.global.support.Cursor;
-import kkomo.global.support.CursorServiceSupport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MemberResponseCursorService extends CursorServiceSupport {
+public class MemberResponseCursorService {
 
     private final Base64Encoder base64Encoder;
 
     public String encodeCursor(final Slice<MemberResponse> slice) {
-        final Cursor cursor = provideCursor(
-            slice,
-            s -> {
-                final Long id = s.getContent()
-                    .stream()
-                    .reduce((first, second) -> second)
-                    .map(MemberResponse::memberId)
-                    .orElse(null);
-                return Cursor.from(id);
-            }
-        );
+        final List<MemberResponse> content = slice.getContent();
+        if (content.isEmpty()) {
+            return null;
+        }
+        final Long id = content.getLast().memberId();
+        final Cursor cursor = Cursor.from(id);
         return base64Encoder.encode(cursor);
     }
 }
