3a926f15b7faf64c511dce44084c678447f174f4
bokyeong <wntjdals0412@gmail.com>
2025-03-09 15:18:08 +0900

    KM-37 feat : activated 권한 판단 admin도 판단되게 수정

--- src/main/java/kkomo/member/domain/MemberRole.java
+++ src/main/java/kkomo/member/domain/MemberRole.java
@@ -4,7 +4,7 @@ public enum MemberRole {
     ROLE_ACTIVATED, ROLE_DEACTIVATED, ROLE_ADMIN;
 
     public boolean isActivated() {
-        return this == ROLE_ACTIVATED;
+        return this != ROLE_DEACTIVATED;
     }
 
     public boolean isAdmin() {
